e7a37b7f4b4c1942ad9b463dc63d83a6d7ec83f5
Noah Moroze <noahm@moroze.com>
2014-03-02 16:19:20 -0500

    Making tips sexier

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import random
 import datetime
 from SportsData import *
@@ -7,14 +7,18 @@ import os
 import collections
 import math
 from util import *
+from tip_templates import *
+import re
 
 app = Flask(__name__, static_url_path='/static')
 @app.route("/")
 def index():
 	return app.send_static_file('templates/index.html')
+
 @app.route("/display-tips")
 def tips():
 	homeTeam, awayTeam, date = getLatestGame("Red Sox")
+
 	f = FanGraphs(os.environ["kimono_api_key"])
 
 	data = f.getData(date.split("T")[0], homeTeam, 2013)
@@ -22,10 +26,28 @@ def tips():
 
 	sortedData = sorted(data, key=lambda k: math.fabs(float(k['wpa'])))
 
-	topPlays = sortedData[-5:]
+	topPlays = sortedData[-1:]
 	tips = []
-	for cosa in topPlays:
-		tips.append(cosa['wpa'] + ":" + cosa['play'].rstrip(".") + " in " + cosa['inning'] + "<br>")
+	for play in topPlays:
+		tip = ""
+		if float(play['wpa']) > 0: 
+			tip = random.choice(team_templates) % (homeTeam, play['inning'])
+		else:
+			tip = random.choice(team_templates) % (awayTeam, play['inning'])
+
+		formattedPlay = play['play'].rstrip(".")
+		
+		#remove parenthesis 
+		#(from http://stackoverflow.com/questions/8713118/remove-content-inside-parenthesis-as-well-as-the-parenthesis-themselves-from-a-p)
+		regEx = re.compile(r'([^\(]*)\([^\)]*\) *(.*)')
+		m = regEx.match(formattedPlay)
+		while m:
+			text = m.group(1) + m.group(2)
+			m = regEx.match(formattedPlay)
+
+		tip += play['play'].rstrip(".")
+		tips.append(tip)
+		#tips.append(cosa['wpa'] + ":" + cosa['play'].rstrip(".") + " in " + cosa['inning'] +)
 	
 	return render_template('displaytips.html', 
 		homeTeam = homeTeam,
